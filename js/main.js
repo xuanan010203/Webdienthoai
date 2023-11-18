eec535cfa85e2d14fd669d5870d0c44572f8ddf4
skyfourhmmm <nguyenthientu413.com>
2023-11-18 22:33:15 +0700

    xong

--- js/main.js
+++ js/main.js
@@ -5,103 +5,239 @@ const products = {
       img: "../img/Product/product_1.webp",
       name: "IPhone 14 Pro Max 128GB",
       price: "23.890.000",
-      start: 4.6,
+      star: 4.6,
       brand: "iphone",
+      listImg: [
+        "../img/iphone-14/iphone-14(1).webp",
+        "../img/iphone-14/iphone-14(2).webp",
+        "../img/iphone-14/iphone-14(3).webp",
+        "../img/iphone-14/iphone-14-4.webp",
+        "../img/iphone-14/iphone-14-5.webp",
+        "../img/iphone-14/iphone-14-6.webp",
+        "../img/iphone-14/iphone-14-7.webp",
+        "../img/iphone-14/iphone-14-8.webp",
+        "../img/iphone-14/iphone-14-9.webp",
+        "../img/iphone-14/iphone-14-10.webp",
+        "../img/iphone-14/iphone-14-11.webp",
+        "../img/iphone-14/iphone-14-12.webp",
+      ],
+      cost: "27.890.000",
+      rv: 100,
     },
     {
       id: 2,
       img: "../img/Product/product_2.webp",
       name: "Samsung Galaxy A34 5G 256GB",
       price: "6.620.000",
-      start: 4.2,
+      star: 4.2,
       brand: "samsung",
+      listImg: [
+        "../img/Samsung-A34/1.webp",
+        "../img/Samsung-A34/2.webp",
+        "../img/Samsung-A34/3.webp",
+        "../img/Samsung-A34/4.webp",
+        "../img/Samsung-A34/5.webp",
+        "../img/Samsung-A34/6.webp",
+        "../img/Samsung-A34/6.webp",
+      ],
+      cost: "8.620.000",
+      rv: 60,
     },
     {
       id: 3,
       img: "../img/Product/product_3.webp",
       name: "OPPO A57 128GB",
       price: "3.330.000",
-      start: 4.2,
+      star: 4.2,
       brand: "oppo",
+      listImg: [
+        "../img/Oppo_A57/OppoA57_1.webp",
+        "../img/Oppo_A57/OppoA57_2.webp",
+        "../img/Oppo_A57/OppoA57_3.webp",
+        "../img/Oppo_A57/OppoA57_4.webp",
+        "../img/Oppo_A57/OppoA57_5.webp",
+        "../img/Oppo_A57/OppoA57_6.webp",
+        "../img/Oppo_A57/OppoA57_7.webp",
+      ],
+      cost: "4.330.000",
+      rv: 60,
     },
     {
       id: 4,
       img: "../img/Product/product_4.webp",
       name: "Xiaomi Redmi Note 12 8GB",
       price: "4.270.000",
-      start: 4.6,
+      star: 4.6,
       brand: "xiaomi",
+      listImg: [
+        "../img/Redmi12/1.webp",
+        "../img/Redmi12/2.webp",
+        "../img/Redmi12/3.webp",
+        "../img/Redmi12/4.webp",
+        "../img/Redmi12/5.webp",
+        "../img/Redmi12/6.webp",
+        "../img/Redmi12/7.webp",
+        "../img/Redmi12/8.webp",
+        "../img/Redmi12/9.webp",
+      ],
+      cost: "6.270.000",
+      rv: 50,
     },
     {
       id: 5,
       img: "../img/Product/product_5.webp",
       name: "iPhone 11 64GB",
       price: "8.640.000",
-      start: 4.1,
+      star: 4.1,
       brand: "iphone",
+      listImg: [
+        "../img/Ip12/1.webp",
+        "../img/Ip12/1.webp",
+        "../img/Ip12/1.webp",
+        "../img/Ip12/1.webp",
+        "../img/Ip12/1.webp",
+        "../img/Ip12/1.webp",
+        "../img/Ip12/1.webp",
+        "../img/Ip12/1.webp",
+        "../img/Ip12/1.webp",
+      ],
+      cost: "10.640.000",
+      rv: 60,
     },
     {
       id: 6,
       img: "../img/Product/product_6.webp",
       name: "Samsung Galaxy S23 5G 128GB",
       price: "15.510.000",
-      start: 4.6,
+      star: 4.6,
       brand: "samsung",
+      listImg: [
+        "../img/SS_S23/1.jpg",
+        "../img/SS_S23/2.jpg",
+        "../img/SS_S23/3.jpg",
+        "../img/SS_S23/4.jpg",
+        "../img/SS_S23/5.jpg",
+        "../img/SS_S23/6.jpg",
+        "../img/SS_S23/7.jpg",
+        "../img/SS_S23/8.jpg",
+        "../img/SS_S23/9.jpg",
+        "../img/SS_S23/10.jpg",
+        "../img/SS_S23/11.jpg",
+      ],
+      cost: "17.510.000",
+      rv: 30,
     },
     {
       id: 7,
       img: "../img/Product/product_7.webp",
       name: "Xiaomi 13 Lite 5G",
       price: "7.980.000",
-      start: 4.1,
+      star: 4.1,
       brand: "xiaomi",
+      listImg: [
+        "../img/Xiaomi13/1.webp",
+        "../img/Xiaomi13/2.webp",
+        "../img/Xiaomi13/3.webp",
+        "../img/Xiaomi13/4.webp",
+        "../img/Xiaomi13/5.webp",
+        "../img/Xiaomi13/6.webp",
+        "../img/Xiaomi13/7.webp",
+        "../img/Xiaomi13/8.webp",
+        "../img/Xiaomi13/9.webp",
+        "../img/Xiaomi13/10.webp",
+      ],
+      cost: "9.980.000",
+      rv: 40,
     },
     {
       id: 8,
       img: "../img/Product/product_8.webp",
       name: " Vivo Y16 128GB",
       price: "3.870.000",
-      start: 3.9,
+      star: 3.9,
       brand: "vivo",
+      listImg: [
+        "../img/Vivo_Y16/1.webp",
+        "../img/Vivo_Y16/2.webp",
+        "../img/Vivo_Y16/3.webp",
+        "../img/Vivo_Y16/4.webp",
+      ],
+      cost: "5.870.000",
+      rv: 40,
     },
     {
       id: 9,
       img: "../img/Product/product_9.webp",
       name: "realme C55 6GB",
       price: "23.890.000",
-      start: 4.6,
+      star: 4.6,
       brand: "realme",
+      listImg: [
+        "../img/realme_C55/1.webp",
+        "../img/realme_C55/2.webp",
+        "../img/realme_C55/3.webp",
+        "../img/realme_C55/4.webp",
+      ],
+      cost: "25.890.000",
+      rv: 30,
     },
     {
       id: 10,
       img: "../img/Product/product_10.webp",
-      name: " realme C55 6GB",
+      name: " realme Y55 6GB",
       price: "3.870.000",
-      start: 4.6,
+      star: 4.6,
       brand: "realme",
+      listImg: [
+        "../img/realme_C55/1.webp",
+        "../img/realme_C55/2.webp",
+        "../img/realme_C55/3.webp",
+        "../img/realme_C55/4.webp",
+      ],
+      cost: "5.870.000",
+      rv: 30,
     },
     {
       id: 11,
       img: "../img/Product/product_11.webp",
       name: "Samsung Galaxy A14 6GB",
       price: "3.460.000",
-      start: 4.6,
+      star: 4.6,
       brand: "samsung",
+      listImg: [
+        "../img/SS_A14/1.webp",
+        "../img/SS_A14/2.webp",
+        "../img/SS_A14/3.webp",
+        "../img/SS_A14/4.webp",
+        "../img/SS_A14/5.webp",
+        "../img/SS_A14/6.webp",
+      ],
+      cost: "6.460.000",
+      rv: 40,
     },
     {
       id: 12,
       img: "../img/Product/product_12.webp",
       name: "Samsung Galaxy A13 6GB",
       price: "2.920.000",
-      start: 4.6,
+      star: 4.6,
       brand: "samsung",
+      listImg: [
+        "../img/SS_A13/1.webp",
+        "../img/SS_A13/2.webp",
+        "../img/SS_A13/3.webp",
+        "../img/SS_A13/4.webp",
+        "../img/SS_A13/5.webp",
+      ],
+      cost: "4.920.000",
+      rv: 30,
     },
     {
       id: 13,
       img: "../img/Product/product_13.webp",
       name: "OPPO Find N2 Flip",
       price: "19.990.000",
-      start: 4.6,
+      star: 4.6,
       brand: "oppo",
     },
     {
@@ -109,68 +245,143 @@ const products = {
       img: "../img/Product/product_14.webp",
       name: "Xiaomi Redmi 12C",
       price: "2.990.000",
-      start: 4.6,
+      star: 4.6,
       brand: "xiaomi",
+      listImg: [
+        "../img/Redmi_12C/1.webp",
+        "../img/Redmi_12C/2.webp",
+        "../img/Redmi_12C/3.webp",
+        "../img/Redmi_12C/4.webp",
+        "../img/Redmi_12C/5.webp",
+      ],
+      cost: "4.990.000",
+      rv: 20,
     },
     {
       id: 15,
       img: "../img/Product/product_15.webp",
       name: "Samsung Galaxy S23 Ultra 5",
       price: "26.990.000",
-      start: 4.6,
+      star: 4.6,
       brand: "samsung",
+      listImg: [
+        "../img/S23_Ultra/1.webp",
+        "../img/S23_Ultra/2.webp",
+        "../img/S23_Ultra/3.webp",
+        "../img/S23_Ultra/4.webp",
+        "../img/S23_Ultra/5.webp",
+        "../img/S23_Ultra/6.webp",
+        "../img/S23_Ultra/7.webp",
+      ],
+      cost: "26.990.000",
+      rv: 20,
     },
     {
       id: 16,
       img: "../img/Product/product_16.webp",
       name: "Vivo Y35",
       price: "6.290.000",
-      start: 4.6,
+      star: 4.6,
       brand: "vivo",
+      listImg: [
+        "../img/Vivo_Y35/1.webp",
+        "../img/Vivo_Y35/2.webp",
+        "../img/Vivo_Y35/3.webp",
+        "../img/Vivo_Y35/4.webp",
+      ],
+      cost: "8.290.000",
+      rv: 20,
     },
     {
       id: 17,
       img: "../img/Product/product_17.webp",
       name: "Realme C30s",
       price: "1.890.000",
-      start: 4.6,
+      star: 4.6,
       brand: "realme",
+      listImg: [
+        "../img/Realme_C30s/1.webp",
+        "../img/Realme_C30s/2.webp",
+        "../img/Realme_C30s/3.webp",
+        "../img/Realme_C30s/4.webp",
+        "../img/Realme_C30s/5.webp",
+        "../img/Realme_C30s/6.webp",
+      ],
+      cost: "3.890.000",
+      rv: 20,
     },
     {
       id: 18,
       img: "../img/Product/product_18.webp",
       name: "Samsung Galaxy S23+ 5G",
       price: "22.990.000",
-      start: 4.6,
+      star: 4.6,
       brand: "samsung",
+      listImg: [
+        "../img/S23+/1.webp",
+        "../img/S23+/2.webp",
+        "../img/S23+/3.webp",
+        "../img/S23+/4.webp",
+        "../img/S23+/5.webp",
+        "../img/S23+/6.webp",
+        "../img/S23+/7.webp",
+      ],
+      cost: "25.990.000",
+      rv: 20,
     },
     {
       id: 19,
       img: "../img/Product/product_19.webp",
       name: "Samsung Galaxy Z Fold4 5G",
       price: "34.990.000",
-      start: 4.6,
+      star: 4.6,
       brand: "samsung",
+      listImga: [
+        "../img/ZFold/1.webp",
+        "../img/ZFold/2.webp",
+        "../img/ZFold/3.webp",
+        "../img/ZFold/4.webp",
+        "../img/ZFold/5.webp",
+        "../img/ZFold/6.webp",
+      ],
+      cost: "36.990.000",
+      rv: 20,
     },
     {
       id: 20,
       img: "../img/Product/product_20.webp",
       name: "TCL 30+",
       price: "3.190.000",
-      start: 4.6,
+      star: 4.6,
       brand: "nokia",
+      listImg: [
+        "../img/TCL_30+/1.webp",
+        "../img/TCL_30+/2.webp",
+        "../img/TCL_30+/3.webp",
+        "../img/TCL_30+/4.webp",
+      ],
+      cost: "5.190.000",
+      rv: 10,
     },
     {
       id: 21,
       img: "../img/Product/product_21.webp",
       name: "OPPO A16K",
       price: "1.890.000",
-      start: 4.6,
+      star: 4.6,
       brand: "oppo",
+      listImg: [
+        "../img/OPPO_A16K/1.webp",
+        "../img/OPPO_A16K/2.webp",
+        "../img/OPPO_A16K/3.webp",
+        "../img/OPPO_A16K/4.webp",
+        "../img/OPPO_A16K/5.webp",
+      ],
+      cost: "1.890.000",
+      rv: 10,
     },
   ],
 };
-
 document.addEventListener("DOMContentLoaded", () => {
   const addHTML = async () => {
     // Hàm fetch và cập nhật header
@@ -194,8 +405,9 @@ document.addEventListener("DOMContentLoaded", () => {
     await fetchFooter();
 
     // Sau khi header và footer được tải xong, bạn có thể gọi các hàm khác ở đây
-    Home();
     Login();
+    Home();
+    Product_All();
     Search();
   };
 
@@ -209,36 +421,55 @@ const Home = async () => {
 
   const items_1 = products.product.map((item, index) => {
     if (index <= 4) {
-      return `<li class="list-group-item" id=${index}>
-      
+      return `<li class="list-group-item" id=${item.id}>
+    
       <img
         style="width: 207px; height: 207px"
         src=${item.img}
         alt=""
-        
+    <div class="content-items">
+      <h6 >${item.name}</h6>
+      <strong class="price text-danger"> ${item.price} VNĐ </strong>
+      <p class="text-decoration-line-through">${item.price} VNĐ</p>
+      <div class="star">Đánh giá: ${item.star}</div>
+    </div>
   </li>`;
     }
   });
 
   const items_2 = products.product.map((item, index) => {
     if (index >= 5 && index <= 9) {
-      return `<li class="list-group-item">
-        <img
-          style="width: 207px; height: 207px"
-          src=${item.img}
-          alt=""
-    </li>`;
+      return `<li class="list-group-item" id=${item.id}>
+    
+      <img
+        style="width: 207px; height: 207px"
+        src=${item.img}
+        alt=""
+    <div class="content-items">
+      <h6 >${item.name}</h6>
+      <strong class="price text-danger"> ${item.price} VNĐ </strong>
+      <p class="text-decoration-line-through">${item.price} VNĐ</p>
+      <div class="star">Đánh giá: ${item.star}</div>
+    </div>
+  </li>`;
     }
   });
 
   const items_3 = products.product.map((item, index) => {
     if (index >= 10 && index <= 14) {
-      return `<li class="list-group-item">  
-        <img
-          style="width: 207px; height: 207px"
-          src=${item.img}
-          alt="" 
-    </li>`;
+      return `<li class="list-group-item" id=${item.id}>
+    
+      <img
+        style="width: 207px; height: 207px"
+        src=${item.img}
+        alt=""
+    <div class="content-items">
+      <h6 >${item.name}</h6>
+      <strong class="price text-danger"> ${item.price} VNĐ </strong>
+      <p class="text-decoration-line-through">${item.price} VNĐ</p>
+      <div class="star">Đánh giá: ${item.star}</div>
+    </div>
+  </li>`;
     }
   });
 
@@ -250,7 +481,37 @@ const Home = async () => {
   productList3.innerHTML = itemsHTML_3;
 
   const productItems_1 = document.querySelectorAll(".product_list_1 li");
+  const productItems_2 = document.querySelectorAll(".product_list_2 li");
+  const productItems_3 = document.querySelectorAll(".product_list_3 li");
+
   productItems_1.forEach((e) => {
+    e.addEventListener("click", (event) => {
+      // Chuyển đối tượng thành chuỗi JSON và mã hóa chuỗi
+      var encodedData = encodeURIComponent(
+        JSON.stringify(products.product[event.currentTarget.id])
+      );
+
+      // Chuyển hướng đến trang mới và truyền dữ liệu qua tham số truy vấn
+      window.location.href =
+        "http://127.0.0.1:5500/html/product_detail.html?data=" + encodedData;
+    });
+  });
+
+  productItems_2.forEach((e) => {
+    e.addEventListener("click", (event) => {
+      // Chuyển đối tượng thành chuỗi JSON và mã hóa chuỗi
+      console.log(products.product[event.currentTarget.id]);
+      var encodedData = encodeURIComponent(
+        JSON.stringify(products.product[event.currentTarget.id])
+      );
+
+      // Chuyển hướng đến trang mới và truyền dữ liệu qua tham số truy vấn
+      // window.location.href =
+      //   "http://127.0.0.1:5500/html/product_detail.html?data=" + encodedData;
+    });
+  });
+
+  productItems_3.forEach((e) => {
     e.addEventListener("click", (event) => {
       // Chuyển đối tượng thành chuỗi JSON và mã hóa chuỗi
       var encodedData = encodeURIComponent(
@@ -269,6 +530,8 @@ const Login = () => {
   const userCurrent = document.querySelector(".login");
   const userLogout = document.querySelector(".logout");
   const userSelect = document.querySelector(".user-select");
+
+  console.log(userCurrent);
 
   let data = JSON.parse(localStorage.getItem("user_info"));
   if (data && data.loged == 1) {
@@ -293,6 +556,67 @@ const Login = () => {
   });
 };
 
+const Cart = () => {
+  let userDataString = localStorage.getItem("user_info");
+  let userData = JSON.parse(userDataString);
+  const productBuy = document.querySelector(".product-buy");
+
+  var productHTMLArray = userData.products.map((e, index) => {
+    return `<div class="row">
+    <div class="col-6 mx-auto mt-4 border d-flex p-3">
+        <input type="radio" value=""> 
+        <img src=${e.img} alt="" class="mx-2" style="width: 120px;">
+        <div class="pay-info d-flex flex-column mx-4">
+            <span class="fs-6">${e.name}</span>
+            <div>
+                <span class="text-danger fs-6">${e.price}</span>
+                <span style="text-decoration: line-through; font-size: 12px;">${e.cost}</span>
+            </div>
+        </div>
+        <div class="icon-pay text-end">
+            <i class="fa-solid fa-trash"></i>
+            <div class="input-group input-group-sm border rounded-2 w-100">
+                <!-- Nút trừ -->
+                <div class="input-group-prepend">
+                    <button
+                        type="button"
+                        class="btn btn-default btn-number btn-sm"
+                        data-type="minus"
+                        data-field="quantity"
+                    >
+                        <i class="fas fa-minus"></i>
+                    </button>
+                </div>
+            
+                <!-- Nhập -->
+                <input
+                    type="text"
+                    name="quantity"
+                    class="form-control input-number input-focus-none text-center"
+                    value=${e.quantity}
+                    min="1"
+                    readonly
+                />
+            
+                <!-- Nút cộng -->
+                <div class="input-group-append">
+                    <button
+                        type="button"
+                        class="btn btn-default btn-number btn-sm"
+                        data-type="plus"
+                        data-field="quantity"
+                    >
+                        <i class="fas fa-plus"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`;
+  });
+  productBuy.insertAdjacentHTML("beforeend", productHTMLArray.join(""));
+};
+
 // ---search
 
 const Search = () => {
@@ -305,4 +629,43 @@ const Search = () => {
     });
   });
 };
+
+const Product_All = () => {
+  const productList_all = document.querySelector(".product_list");
+
+  const items_all = products.product.map((item, index) => {
+    return `<li class="list-group-item" id=${item.id}>
+    
+      <img
+        style="width: 207px; height: 207px"
+        src=${item.img}
+        alt=""
+    <div class="content-items">
+      <h6 >${item.name}</h6>
+      <strong class="price text-danger"> ${item.price} VNĐ </strong>
+      <p class="text-decoration-line-through">${item.price} VNĐ</p>
+      <div class="star">Đánh giá: ${item.star}</div>
+    </div>
+  </li>`;
+  });
+
+  const itemsHTML_all = items_all.join("");
+  productList_all.innerHTML = itemsHTML_all;
+
+  const itemsProduct = document.querySelectorAll(".product_list li");
+  itemsProduct.forEach((e) => {
+    e.addEventListener("click", (event) => {
+      // Chuyển đối tượng thành chuỗi JSON và mã hóa chuỗi
+      var encodedData = encodeURIComponent(
+        JSON.stringify(products.product[event.currentTarget.id])
+      );
+      console.log(event.currentTarget.id);
+
+      // Chuyển hướng đến trang mới và truyền dữ liệu qua tham số truy vấn
+      window.location.href =
+        "http://127.0.0.1:5500/html/product_detail.html?data=" + encodedData;
+    });
+  });
+};
 //--- call funcition
+Cart();
